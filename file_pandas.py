d378e8a459cff144852ce6619edb13126604f01b
gdgski <gregorio.degiuli2@gmail.com>
2025-02-27 14:51:17 +0100

    Optimised code
    Removed recreation of dataframe, and imported from dataframe from file CSV cleaning

--- file_pandas.py
+++ file_pandas.py
@@ -5,34 +5,7 @@ from xmlrpc.client import DateTime
 from pprint import pprint
 import pandas as pd
 import numpy as np
-
-# Create a dictionary to then create a dataframe
-with open('TSLA_modified.csv',"r", encoding='utf-8') as f:
-    lettore = csv.reader(f, delimiter=',')
-    f.readline()
-    price_data = []
-    for riga in lettore:
-        price_data.append(riga)
-# Creation of the dataframe
-dataframe_prices = pd.DataFrame(price_data, columns=["Date","Open","High","Low","Close","Adj Close","Volume"])
-dataframe_prices.replace("", np.nan, inplace=True)
-#changing the type of datas
-dataframe_prices['Date'] = pd.to_datetime(dataframe_prices['Date'])
-dataframe_prices['Open'] = dataframe_prices['Open'].astype(float)
-dataframe_prices['Close'] = dataframe_prices['Close'].astype(float)
-dataframe_prices['Low'] = dataframe_prices['Low'].astype(float)
-dataframe_prices['High'] = dataframe_prices['High'].astype(float)
-dataframe_prices['Adj Close'] = dataframe_prices['Adj Close'].astype(float)
-dataframe_prices['Volume'] = dataframe_prices['Volume'].astype(float)
-
-#drop the NaNs from the dataframe
-df_cleared = dataframe_prices.dropna(axis = 0, subset = ["Date","Open","High","Low","Close","Adj Close","Volume"])
-
-#counting the NaNs after clearing
-total_nansx = df_cleared.isna().sum().sum()
-# print(total_nansx)
-df_cleaned = df_cleared.dropna().reset_index(drop=True)
-# print(df_cleaned)
+from CSV_cleaning import df_cleaned
 
 #creation of a dataframe called final_file with only the first 700 rows
 final_file = df_cleaned.head(701)
---
+++ file_pandas2.py
@@ -0,0 +1,9 @@
+import csv
+from matplotlib import pyplot as plt
+from typing import final
+from xmlrpc.client import DateTime
+from pprint import pprint
+import pandas as pd
+import numpy as np
+
+from CSV_cleaning import df_cleaned
