cd110ce7791e43d20dcd6c73beba8a4b9977e837
marco904 <marcooccoffer@gmail.com>
2025-02-27 11:06:55 +0100

    first step
    first step of the part for pandas

--- file_pandas.py
+++ file_pandas.py
@@ -1,4 +1,5 @@
 import csv
+from typing import final
 from xmlrpc.client import DateTime
 from pprint import pprint
 import pandas as pd
@@ -30,7 +31,28 @@ df_cleared = dataframe_prices.dropna(axis = 0, subset = ["Date","Open","High","L
 total_nansx = df_cleared.isna().sum().sum()
 # print(total_nansx)
 df_cleaned = df_cleared.dropna().reset_index(drop=True)
-print(df_cleaned)
+# print(df_cleaned)
 
-# Load the first 700 rows of the CSV file
-# print(df_cleaned, nrows=700)
+#creation of a dataframe called final_file with only the first 700 rows
+final_file = df_cleaned.head(701)
+# print(final_file)
+
+# Check if for all rows the following statements are true:
+#creation of a def to check if the statement condition is True
+def verifica(condition):
+    if not condition.all():
+        print("valore errato")
+    else:
+        print("okey")
+    # The High value is greater than or equal to both the Open and the Close values.
+# The High value is greater than or equal to both the Open and the Close values.
+condition1 = (final_file['High'] >= final_file['Open']) & (final_file['High'] >= final_file['Close'])
+verifica(condition1)
+# # The Low value is less than or equal to both the Open and the Close values
+condition2 = (final_file['Low'] <= final_file['Open']) & (final_file['Low'] <= final_file['Close'])
+verifica(condition2)
+ # The Adj Close values is less than or equal to the Close value
+condition3 = (final_file['Adj Close'] <= final_file['Close'])
+verifica(condition3)
+
+#aggiungere una riga e testare
