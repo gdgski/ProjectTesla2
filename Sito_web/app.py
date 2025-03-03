f999c403ebf8cc1ac99dd4c4e64f75b9725186e1
EdoLucca <edoardolucca1@gmail.com>
2025-03-03 12:00:15 +0100

    contatti

--- Sito_web/app.py
+++ Sito_web/app.py
@@ -34,22 +34,15 @@ def visualizza_dati():
 def modelli():
     return render_template("modelli.html")
 
-# @app.route("/visualizza2")
-# def visualizza_dati2():
-#     # Lista di dati base in Python
-#     dati_finanziari = [
-#         {'date': '2010-07-01', 'open': 9999, 'high': 5.184, 'low': 4.054, 'close': 4.392, 'adj_close': 4.392,
-#          'volume': 41094000},
-#         {'date': '2010-07-02', 'open': 4.6, 'high': 4.62, 'low': 3.742, 'close': 3.84, 'adj_close': 3.84,
-#          'volume': 25699000},
-#         {'date': '2010-07-06', 'open': 4.0, 'high': 4.0, 'low': 3.166, 'close': 3.222, 'adj_close': 3.222,
-#          'volume': 34334500},
-#         {'date': '2010-07-07', 'open': 3.28, 'high': 3.326, 'low': 2.996, 'close': 3.16, 'adj_close': 3.16,
-#          'volume': 34608500},
-#         {'date': '2010-07-08', 'open': 3.2, 'high': 3.3, 'low': 3.05, 'close': 3.1, 'adj_close': 3.1,
-#          'volume': 29987000},
-#     ]
-#     return render_template("visualizza_dati2.html",data = dati_finanziari)
+@app.route("/contatti")
+def contatti():
+    return render_template("contatti.html")
+
+@app.route("/tecnologia")
+def tecnologia():
+    return render_template("tecnologia.html")
+
+
 
 
 if __name__ == "__main__":
